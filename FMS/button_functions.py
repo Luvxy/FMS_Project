7f1b995f06271be398ee2b5e43d8691f07955864
Luvxy <luvxy107@handong.ac.kr>
2023-11-03 15:56:54 +0900

    Ui 파일 편집, 버튼 기능 분리
    Ui 파일 편집
    - Ui 파일 생성
    - Ui 파일 .ui -> .py 변환
    
    버튼 기능 분리
    - 버튼 함수 파일 생성
    - Ui와 버튼 연결

---
+++ FMS/button_functions.py
@@ -0,0 +1,12 @@
+# button_functions.py
+
+def on_button1_clicked():
+    print("Button 1 clicked")
+
+def on_button2_clicked():
+    print("Button 2 clicked")
+
+def on_button3_clicked():
+    print("Button 3 clicked")
+
+# Add more functions for other buttons if needed
---
+++ FMS/pyqt_test.py
@@ -0,0 +1,23 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_file import Ui_MainWindow  # Replace 'ui_file' with the actual module name.
+from button_functions import on_button1_clicked, on_button2_clicked, on_button3_clicked
+
+class MyMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Connect button clicks to functions
+        self.ui.pushButton.clicked.connect(on_button1_clicked)
+        self.ui.pushButton_2.clicked.connect(on_button2_clicked)
+        self.ui.pushButton_3.clicked.connect(on_button3_clicked)
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = MyMainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
